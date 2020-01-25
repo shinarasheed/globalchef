fb1d73696c40f8a4a30ab9fddb1ff4262b6e473d
User <rasheedshinaopeyemi@gmail.com>
2020-01-25 20:33:59 +0100

    finished classes

---
+++ src/includes/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="nav-brand" to="/">
+          <img src={require("../assets/images/logo.png")} alt="" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav  mt-2 mt-lg-0 links">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/classes">
+                Classes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav mt-2 mt-lg-0 signup">
+            <li className="nav-item active">
+              <Link className="nav-link signup" to="#">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
