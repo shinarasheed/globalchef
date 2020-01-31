9c6886d0e1d67143e266e7a3351b23ff27835694
Olabanji121 <olabanjiabimbola@gmail.com>
2020-01-31 18:57:51 +0100

    NavBar

--- client/src/includes/Navbar.js
+++ client/src/includes/Navbar.js
@@ -1,8 +1,114 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import {logout} from '../actions/auth'
-const Navbar = ({logout}) => {
+const Navbar = ({auth: { isAuthenticated, loading, user} ,logout}) => {
+
+  const authLinks = (
+   
+  
+    <ul className="navbar-nav  mt-2 mt-lg-0 links">
+      <li className="nav-item active">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/enrolled">
+          Enrolled
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/learning">
+          Learning
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/classes">
+          Classes
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/saved">
+          Saved
+        </Link>
+      </li>
+
+      <div className="navbar-nav mt-2 mt-lg-0 signup">
+    
+      <li className="nav-item active">
+        <a onClick={logout} href="#!" className="nav-link signup">
+          Logout
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <Link className="nav-link" to="/enrolled">
+          <div className="row">
+            <div className="col">
+            {user && user.name.split(' ')[0]}
+            </div>
+            <div className="col pb-2">
+            <img src={user && user.avatar} alt="user" style={{width: '50px'}} className="round-img"/>
+            </div>
+           
+            </div>
+         
+        </Link>
+      </li>
+      <li>
+      
+      </li>
+      
+    </div>
+    </ul>
+
+    
+  
+  
+  )
+
+  const guestLinks = (
+   
+    <ul className="navbar-nav  mt-2 mt-lg-0 links">
+      <li className="nav-item active">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+     
+      <li className="nav-item">
+        <Link className="nav-link" to="/learning">
+          Learning
+        </Link>
+      </li>
+     
+      
+
+      <div className="navbar-nav mt-2 mt-lg-0 signup">
+      <li className="nav-item active">
+        <Link className="nav-link signup" to="/signin">
+          Sign In
+        </Link>
+      </li>
+    
+      <li className="nav-item active">
+        <Link
+          className="nav-link signup"
+          to="/signup"
+          style={{ marginLeft: "20px" }}
+        >
+          Sign Up
+        </Link>
+      </li>
+    </div>
+    </ul>
+    
+
+  );
+
+
+
   return (
     <>
       <nav className="navbar navbar-expand-lg">
@@ -21,55 +127,11 @@ const Navbar = ({logout}) => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
+  
+
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav  mt-2 mt-lg-0 links">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/enrolled">
-                Enrolled
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/learning">
-                Learning
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/classes">
-                Classes
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/saved">
-                Saved
-              </Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav mt-2 mt-lg-0 signup">
-            <li className="nav-item active">
-              <Link className="nav-link signup" to="/signin">
-                Sign In
-              </Link>
-            </li>
-            <li className="nav-item active">
-              <a onClick={logout} href="#!" className="nav-link signup">
-                Logout
-              </a>
-            </li>
-            <li className="nav-item active">
-              <Link
-                className="nav-link signup"
-                to="/signup"
-                style={{ marginLeft: "20px" }}
-              >
-                Sign Up
-              </Link>
-            </li>
-          </ul>
+        {!loading &&  (<Fragment>{isAuthenticated? authLinks: guestLinks}</Fragment>)}
+         
         </div>
       </nav>
     </>
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -37,6 +37,7 @@ export default function(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
     case LOGIN_FAIL:
     case LOG_OUT:
       localStorage.removeItem("token");
